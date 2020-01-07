637d2dae121845e7e91e8910475b9413382e992e
Paul Klinger <klinger.paul@gmail.com>
2020-01-07 22:13:05 +0000

    config bugfix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ FAI_PATH = "./data/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.fna.f
 
 VCF_PATH_PATTERN = "./data/grch38_calls_filtered_{contig}.vcf.gz"
 
-CONTIGS = ["chr" + i for i in range(1,23)] + ["chrX", "chrY", "chrM"]
+CONTIGS = ["chr{}".format(i) for i in range(1,23)] + ["chrX", "chrY", "chrM"]
 
 N_LEDS = 9
 LED_PIN_1 = 18
